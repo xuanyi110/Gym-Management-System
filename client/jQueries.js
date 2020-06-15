ead3a34c195db3ca476c4a86c2af29fb2485c0da
tszngai <ngaifu16@gmail.com>
2020-06-14 19:05:42 -0700

    add queries

--- client/jQueries.js
+++ client/jQueries.js
@@ -1,8 +1,30 @@
 var jQ = [
     {
-        text: 'Retrieve Membership ID from Customer ID',
+        text: 'Retrieve Membership ID from customer ID',
         apiName: "cus2mem",
         userInputs: ['customerId']
+    },
+    {
+        text: 'Retrieve monthly salary from staff ID',
+        apiName: "staff2salary",
+        userInputs: ['staffId']
+    },
+    {
+        text: 'List all customer ID',
+        apiName: "projectFrom",
+        inputs: {
+            attr: 'ID',
+            table: 'CUSTOMER'
+        }
+    },
+    {
+        text: 'Retrieve number of personal trainees from coach ID',
+        apiName: "coach2trainee",
+        userInputs: ['coachId']
+    },
+    {
+        text: 'Who have participated in all training courses',
+        apiName: "whoAttendedAll"
     },
     {
         text: 'Retrieve all from',
@@ -10,7 +32,8 @@ var jQ = [
         userInputs: ['table']
     },
     {
-        text: 'count staff',
-        apiName: 'countStaff',
+        text: 'Count all from',
+        apiName: 'countAll',
+        userInputs: ['table']
     }
 ];
