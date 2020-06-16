6cfc8de02486b23248a5551959116fc148c6b56f
tszngai <ngaifu16@gmail.com>
2020-06-16 13:49:02 -0700

    general queries

---
+++ client/generalQueries.js
@@ -0,0 +1,20 @@
+var generalQ = [
+    {
+        text: 'GENERAL - Project attr from table',
+        apiName: "projectFrom",
+        userInputs: ['attr', 'table'],
+        categories: ['projection']
+    },
+    {
+        text: 'GENERAL - Project with conditions',
+        apiName: "projectFromWhere",
+        userInputs: ['attr', 'table', 'condition'],
+        categories: ['selection', 'projection']
+    },
+    {
+        text: 'GENERAL - Join with conditions',
+        apiName: "joinWhere",
+        userInputs: ['attr', 'tables', 'condition'],
+        categories: ['join', 'projection']
+    },
+]
