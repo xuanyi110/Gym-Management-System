a073da1882cf518f88c4f82f80299332f816e9da
Lambert <lambert@Flokas-MacBook-Air.local>
2020-06-15 18:04:23 -0700

    add category and aggregation sql

--- client/lambertQueries.js
+++ client/lambertQueries.js
@@ -8,62 +8,80 @@ var lambertQ = [
             table3: "meal_plan",
             table4: "meal_plan_recommendation",
         },
-        userInputs: ['coach_ID']
+        userInputs: ['coach_ID'],
+        categories: ['projection', 'join', 'selection']
     },
     {
         text: 'get food item detailed information based on meal plan ID',
         apiName: "getFIInByMealPlan",
         inputs: {
         },
-        userInputs: ['meal_plan_ID']
+        userInputs: ['meal_plan_ID'],
+        categories: ['projection', 'join', 'selection']
     },
     {
         text: 'get session detailed information based on its coach ID',
         apiName: "getSessionByCoach",
         inputs: {
         },
-        userInputs: ['coach_ID']
+        userInputs: ['coach_ID'],
+        categories: ['projection', 'join', 'selection']
     },
     {
         text: 'get saled membership detailed information based on its sales ID',
         apiName: "getSaledMemInfoBySalesID",
         inputs: {
         },
-        userInputs: ['sales_ID']
+        userInputs: ['sales_ID'],
+        categories: ['projection', 'join', 'selection']
+
     },
     {
         text: 'get membership modification information by manager ID',
         apiName: "getModInfoByManagerID",
         inputs: {
         },
-        userInputs: ['manager_ID']
+        userInputs: ['manager_ID'],
+        categories: ['projection', 'join', 'selection']
     },
     {
         text: 'get machine information by area ID',
         apiName: "getMachinesByArea",
         inputs: {
         },
-        userInputs: ['area_ID']
+        userInputs: ['area_ID'],
+        categories: ['projection', 'join', 'selection']
     },
     {
         text: 'get all occupied lockers info with its customer names',
         apiName: "getLockerwithCustomer",
         inputs: {
         },
-        userInputs: []
+        userInputs: [],
+        categories: ['projection', 'join', 'selection']
     },
     {
         text: 'get customer information and course information based on given gym session id',
         apiName: "getCustSameSession",
         inputs: {
         },
-        userInputs: ['session_number']
+        userInputs: ['session_number'],
+        categories: ['projection', 'join', 'selection']
     },
     {
         text: 'get total calories in a meal plan by meal plan ID',
         apiName: "getCalByMealPlan",
         inputs: {
         },
-        userInputs: ['meal_plan_ID']
+        userInputs: ['meal_plan_ID'],
+        categories: ['projection', 'join', 'selection', 'nested_aggregation']
+    },
+    {
+        text: 'get number of machines in a area',
+        apiName: "getNumofMachinesinArea",
+        inputs: {
+        },
+        userInputs: ['areaID'],
+        categories: ['selection', 'aggregation']
     }
 ];
