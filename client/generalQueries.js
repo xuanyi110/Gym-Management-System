53945d52ea805867236f2047b29fa628b33d3755
tszngai <ngaifu16@gmail.com>
2020-06-16 17:23:09 -0700

    add agg and nested_agg

--- client/generalQueries.js
+++ client/generalQueries.js
@@ -11,4 +11,16 @@ var generalQ = [
         userInputs: ['attr', 'tables', 'condition'],
         categories: ['selection', 'join', 'projection']
     },
+    {
+        text: 'GENERAL - Aggregate from table (e.g. agg_function=count)',
+        apiName: "aggFrom",
+        userInputs: ['agg_function', 'agg_over_attr', 'table'],
+        categories: ['aggregation']
+    },
+    {
+        text: 'GENERAL - Nested aggregate from table (e.g. agg_function=count)',
+        apiName: "nestedAggFrom",
+        userInputs: ['agg_function', 'agg_over_attr', 'table', 'group_by'],
+        categories: ['aggregation', 'nested_aggregation']
+    },
 ]
