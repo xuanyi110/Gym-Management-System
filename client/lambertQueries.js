5a4c315e00ee0d47880a3d62f8fd083627c831ab
Lambert <lambert@Flokas-MacBook-Air.local>
2020-06-16 17:38:08 -0700

    change category

--- client/lambertQueries.js
+++ client/lambertQueries.js
@@ -1,6 +1,6 @@
 var lambertQ = [
     {
-        text: 'get the customer names, ids and their meal plans based on coach id',
+        text: 'get the customer names, ids and their meal plans based on coach id. (Valid input: C1; C2; C3; C4; C5)',
         apiName: "getCusMealByCoach",
         inputs: {
             table1: "customer",
@@ -9,48 +9,48 @@ var lambertQ = [
             table4: "meal_plan_recommendation",
         },
         userInputs: ['coach_ID'],
-        categories: ['projection', 'join', 'selection']
+        categories: ['selection']
     },
     {
-        text: 'get food item detailed information based on meal plan ID',
+        text: 'get food item detailed information based on meal plan ID. (Valid input: 1; 2; 3; 4; 5)',
         apiName: "getFIInByMealPlan",
         inputs: {
         },
         userInputs: ['meal_plan_ID'],
-        categories: ['projection', 'join', 'selection']
+        categories: ['selection']
     },
     {
-        text: 'get session detailed information based on its coach ID',
+        text: 'get session detailed information based on its coach ID. (Valid input: C1; C2; C3; C4; C5)',
         apiName: "getSessionByCoach",
         inputs: {
         },
         userInputs: ['coach_ID'],
-        categories: ['projection', 'join', 'selection']
+        categories: ['selection']
     },
     {
-        text: 'get saled membership detailed information based on its sales ID',
+        text: 'get saled membership detailed information based on its sales ID. (Valid input: S1; S2; S3; S4; S5)',
         apiName: "getSaledMemInfoBySalesID",
         inputs: {
         },
         userInputs: ['sales_ID'],
-        categories: ['projection', 'join', 'selection']
+        categories: ['selection']
 
     },
     {
-        text: 'get membership modification information by manager ID',
+        text: 'get membership modification information by manager ID. (Valid input: M1; M2; M3; M4; M5)',
         apiName: "getModInfoByManagerID",
         inputs: {
         },
         userInputs: ['manager_ID'],
-        categories: ['projection', 'join', 'selection']
+        categories: ['selection']
     },
     {
-        text: 'get machine information by area ID',
+        text: 'get machine information by area ID. (Valid input: 2; 4; 6)',
         apiName: "getMachinesByArea",
         inputs: {
         },
         userInputs: ['area_ID'],
-        categories: ['projection', 'join', 'selection']
+        categories: ['selection']
     },
     {
         text: 'get all occupied lockers info with its customer names',
@@ -58,30 +58,30 @@ var lambertQ = [
         inputs: {
         },
         userInputs: [],
-        categories: ['projection', 'join', 'selection']
+        categories: ['selection']
     },
     {
-        text: 'get customer information and course information based on given gym session id',
+        text: 'get customer information and course information based on given gym session id. (Valid input: 1; 2; 3; 4; 5)',
         apiName: "getCustSameSession",
         inputs: {
         },
         userInputs: ['session_number'],
-        categories: ['projection', 'join', 'selection']
+        categories: ['selection']
     },
     {
-        text: 'get total calories in a meal plan by meal plan ID',
+        text: 'get total calories in a meal plan by meal plan ID. (Valid input: 1; 2; 3; 4; 5)',
         apiName: "getCalByMealPlan",
         inputs: {
         },
         userInputs: ['meal_plan_ID'],
-        categories: ['projection', 'join', 'selection', 'nested_aggregation']
+        categories: ['nested_aggregation']
     },
     {
-        text: 'get number of machines in a area',
+        text: 'get number of machines in a area. (Valid input: 1; 2; 4; 6)',
         apiName: "getNumofMachinesinArea",
         inputs: {
         },
         userInputs: ['areaID'],
-        categories: ['selection', 'aggregation']
+        categories: ['aggregation']
     }
 ];
