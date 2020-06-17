b2bb640d5758d9e7222549209f74470b11f561f6
tszngai <ngaifu16@gmail.com>
2020-06-17 16:07:00 -0700

    no condition; filter bug; add schema reference

--- client/generalQueries.js
+++ client/generalQueries.js
@@ -6,10 +6,19 @@ var generalQ = [
         categories: ['projection']
     },
     {
-        text: 'GENERAL - Project (and/or join) with conditions',
-        apiName: "projectFromWhere",
-        userInputs: ['attr', 'tables', 'condition'],
-        categories: ['selection', 'join', 'projection']
+        text: 'GENERAL - Selection (e.g. condition_attr=ID, condition_attr_equals=1)',
+        apiName: "projectWhereEqual",
+        userInputs: ['show_attr', 'tables', 'condition_attr', 'condition_attr_equals'],
+        categories: ['selection', 'projection']
+    },
+    {
+        text: 'GENERAL - Join table1 and table2 where attr_of_table1=attr_of_table2',
+        apiName: "joinTwo",
+        userInputs: ['table1', 'table2', 'attr_of_table1', 'attr_of_table2'],
+        categories: ['selection', 'join'],
+        inputs: {
+            show_attr: '*'
+        }
     },
     {
         text: 'GENERAL - Aggregate from table (e.g. agg_function=count)',
